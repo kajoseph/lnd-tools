6bbd11177e6729b272b417f6b0d9f3e21c89e64e
Kenny Joseph <kjoseph@bitpay.com>
2022-09-15 17:59:53 -0400

    fix deterministic builds

--- build.sh
+++ build.sh
@@ -1,24 +1,8 @@
 #!/usr/bin/env bash
 
-
-CWD=$(dirname $(readlink -f "$0"))
-cd $CWD
-
-ENGINE_VERSION=$(cat package.json | grep '"node":' | sed -r 's/[[:space:]]*["a-z:]*//gi')
-EXISTS=$(which node)
-
-if [ -z $EXISTS ]; then
-  echo Node is not installed. Node $ENGINE_VERSION required.
-  exit;
-fi
-
-
-NODE_VERSION=$(node --version)
-if [ $(echo $NODE_VERSION | sed -r 's/v16.*/v16/') != "v$ENGINE_VERSION" ]; then
-  echo Node v$ENGINE_VERSION required.
-  exit;
-fi
-
+# =================
+# FUNCTIONS
+# =================
 
 Usage () {
   echo "Usage: ./build.sh [options]"
@@ -60,6 +44,7 @@ transformArch () {
 
   if 
     [ "$PARAM" == "x64" ] ||
+    [ "$PARAM" == "amd64" ] ||
     [ "$PARAM" == "x86_64" ] ||
     [ "$PARAM" == "x86" ];
   then
@@ -79,35 +64,69 @@ transformArch () {
 # Darwin mbp-0418 21.6.0 Darwin Kernel Version 21.6.0: Wed Aug 10 14:28:35 PDT 2022; root:xnu-8020.141.5~2/RELEASE_ARM64_T8101 arm64
 # Linux bitpay-kenny-xps 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux
 
-buildForSelf () {
-  echo Building for your system
+getLocalInfo() {
   echo Determining your system specs
+
   SYSTEM_INFO=$(uname -a)
+  echo $SYSTEM_INFO
   SI_ARR=(${SYSTEM_INFO// / })
 
   if [ "${SI_ARR[0]}" == "Darwin" ]; then
-    OS_ARR=(macos)
-    ARCH=${SI_ARR[-1]}
+    MY_OS=macos
+    MY_ARCH=${SI_ARR[-1]}
   elif [ "${SI_ARR[0]}" == "Linux" ]; then
-    OS_ARR=(linux)
-    ARCH=${SI_ARR[-2]}
-  else
+    MY_OS=linux
+    MY_ARCH=${SI_ARR[-2]}
+  fi
+
+  transformArch $MY_ARCH
+  MY_ARCH=$ARCH_T # transformed
+}
+
+buildForSelf () {
+  echo Building for your system
+
+  if [ $MY_OS != "macos" ] && [ $MY_OS != "linux" ]; then
     echo Unknown OS: ${SI_ARR[0]}
     exit 0
   fi
 
-  # make ARCH lowecase
-  ARCH=$(echo "$ARCH" | tr '[:upper:]' '[:lower:]')
-
-  if [ "$ARCH" == "x86_64" ]; then
-    ARCH_ARR=(x64)
-  elif [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
-    ARCH_ARR=(arm64)
-  else
+  if [ $MY_ARCH != "x64" ] && [ $MY_ARCH != "arm65" ]; then
     echo Unknown architecture: $ARCH
     exit 0
   fi
+
+  OS_ARR=($MY_OS)
+  ARCH_ARR=($MY_ARCH)
 }
+
+
+# ===============
+# /FUNCTIONS
+# ===============
+
+
+CWD=$(dirname $(readlink -f "$0"))
+cd $CWD
+
+ENGINE_VERSION=$(cat package.json | grep '"node":' | sed -r 's/[[:space:]]*["a-z:]*//gi')
+EXISTS=$(which node)
+
+if [ -z $EXISTS ]; then
+  echo Node is not installed. Node $ENGINE_VERSION required.
+  exit;
+fi
+
+
+NODE_VERSION=$(node --version)
+if [ $(echo $NODE_VERSION | sed -r 's/v16.*/v16/') != "v$ENGINE_VERSION" ]; then
+  echo Node v$ENGINE_VERSION required.
+  exit;
+fi
+
+echo Using Node $NODE_VERSION
+
+getLocalInfo
 
 # ===============
 # Parse input
@@ -174,40 +193,6 @@ for arch in ${ARCH_ARR[@]}; do
   fi
 done
 
-
-# =========================
-# Create .temp dir with dependencies
-
-if [ -e .temp ]; then
-  echo Removing existing .temp dir
-  rm -rf .temp
-fi
-
-echo Making .temp
-mkdir .temp
-cp package.json .temp
-cp package-lock.json .temp
-cp *.js .temp
-cp -r server .temp
-cp -r client .temp
-
-cd .temp
-
-echo Installing dependencies
-npm i --omit=dev
-
-cd $CWD
-
-# =========================
-# Build the binaries
-
-if [ -e build ]; then
-  cd build
-else
-  mkdir build
-  cd build
-fi
-
 for os in ${OS_ARR[@]}; do
   transformOS $os
   for arch in ${ARCH_ARR[@]}; do
@@ -217,6 +202,39 @@ for os in ${OS_ARR[@]}; do
     OUTPUT=lnd-tools-$OS_T-$ARCH_T
     echo
     echo Building $OUTPUT
+
+    # =========================
+    # Create .temp dir with dependencies
+
+    if [ -e .temp ]; then
+      echo Removing existing .temp dir
+      rm -rf .temp
+    fi
+
+    echo Making .temp
+    mkdir .temp
+    cp package.json .temp
+    cp package-lock.json .temp
+    cp *.js .temp
+    cp -r server .temp
+    cp -r client .temp
+
+    cd .temp
+
+    echo Installing dependencies
+    npm i --omit=dev
+
+    cd $CWD
+
+    # =========================
+    # Build the binaries
+
+    if [ -e build ]; then
+      cd build
+    else
+      mkdir build
+      cd build
+    fi
 
     # reminder: this is being run in the $CWD/build folder
     CMD="../node_modules/.bin/pkg ../.temp/lnd-tools.js \
@@ -233,15 +251,17 @@ for os in ${OS_ARR[@]}; do
     fi
 
     $CMD
+
+    # cd back to cwd
+    cd $CWD
+
+    echo Cleaning up
+    rm -rf .temp
   done
 done
 
 # cd back to cwd
 cd $CWD
-
-echo
-echo Removing .temp folder
-rm -rf .temp
 
 cp ./tls.sh ./build
 
